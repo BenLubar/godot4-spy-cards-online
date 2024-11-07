6dceadde8ba54d3d4feb185a800aa14e9d7ba052
Ben Lubar <ben.lubar@gmail.com>
2024-11-07 13:27:56 -0600

    fix code that was relying on a type alias

--- src/variant_def.cpp
+++ src/variant_def.cpp
@@ -4,10 +4,10 @@ void VariantDef::_bind_methods() {
 	BIND_PROPERTY(Variant::STRING, title);
 	BIND_PROPERTY(Variant::INT, player_count);
 	BIND_PROPERTY_ENUM_ARRAY(enums::NPCDef::NPC, npcs);
-	BIND_PROPERTY_RESOURCE_ARRAY(JigsawTrigger, triggers);
+	BIND_PROPERTY_RESOURCE_ARRAY(JigsawTriggerVariant, triggers);
 }
 
 IMPLEMENT_PROPERTY(VariantDef, String, title);
 IMPLEMENT_PROPERTY(VariantDef, int64_t, player_count);
 IMPLEMENT_PROPERTY(VariantDef, TypedArray<enums::NPCDef::NPC>, npcs);
-IMPLEMENT_PROPERTY(VariantDef, TypedArray<JigsawTrigger>, triggers);
+IMPLEMENT_PROPERTY(VariantDef, TypedArray<JigsawTriggerVariant>, triggers);
