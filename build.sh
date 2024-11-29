f09d2ea0f50f40ee61c638913fd5592f0b780c6d
Ben Lubar <ben.lubar@gmail.com>
2024-11-28 18:05:06 -0600

    build cache

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 
 # build native debug build for this system
-scons dev_build=yes "$@"
+SCONSCACHE=.sconsbuildcache/dev scons dev_build=yes "$@"
 # ...so we can update the docs
 ./update_docs.sh
 # build native debug again to update the embedded docs
-scons dev_build=yes "$@"
+SCONSCACHE=.sconsbuildcache/dev scons dev_build=yes "$@"
 
 # now build all the versions we're actually going to use:
-for target in template_debug template_release; do
-scons target=$target platform=linux arch=x86_64 "$@"
-scons target=$target platform=windows arch=x86_64 "$@"
-scons target=$target platform=web threads=yes "$@"
-scons target=$target platform=web threads=no "$@"
+for target in debug release; do
+SCONSCACHE=.sconsbuildcache/linux_$target scons target=template_$target platform=linux arch=x86_64 "$@"
+SCONSCACHE=.sconsbuildcache/windows_$target scons target=template_$target platform=windows arch=x86_64 "$@"
+SCONSCACHE=.sconsbuildcache/web_threads_$target scons target=template_$target platform=web threads=yes "$@"
+SCONSCACHE=.sconsbuildcache/web_nothreads_$target scons target=template_$target platform=web threads=no "$@"
 done
