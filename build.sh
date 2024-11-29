bebc55da4b7d5822852440b9304048f86b4e6571
Ben Lubar <ben.lubar@gmail.com>
2024-11-28 19:24:38 -0600

    typo in environment variable + change directory so it's ignored in godot-cpp

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 
 # build native debug build for this system
-SCONSCACHE=.sconsbuildcache/dev scons dev_build=yes "$@"
+SCONS_CACHE=.cache/dev scons dev_build=yes "$@"
 # ...so we can update the docs
 ./update_docs.sh
 # build native debug again to update the embedded docs
-SCONSCACHE=.sconsbuildcache/dev scons dev_build=yes "$@"
+SCONS_CACHE=.cache/dev scons dev_build=yes "$@"
 
 # now build all the versions we're actually going to use:
 for target in debug release; do
-SCONSCACHE=.sconsbuildcache/linux_$target scons target=template_$target platform=linux arch=x86_64 "$@"
-SCONSCACHE=.sconsbuildcache/windows_$target scons target=template_$target platform=windows arch=x86_64 "$@"
-SCONSCACHE=.sconsbuildcache/web_threads_$target scons target=template_$target platform=web threads=yes "$@"
-SCONSCACHE=.sconsbuildcache/web_nothreads_$target scons target=template_$target platform=web threads=no "$@"
+SCONS_CACHE=.cache/linux_$target scons target=template_$target platform=linux arch=x86_64 "$@"
+SCONS_CACHE=.cache/windows_$target scons target=template_$target platform=windows arch=x86_64 "$@"
+SCONS_CACHE=.cache/web_threads_$target scons target=template_$target platform=web threads=yes "$@"
+SCONS_CACHE=.cache/web_nothreads_$target scons target=template_$target platform=web threads=no "$@"
 done
