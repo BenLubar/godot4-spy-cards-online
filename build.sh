0d0c13cb0e87a85810bcc625c9d5a1b6c83b66e7
Ben Lubar <ben.lubar@gmail.com>
2025-01-26 20:14:35 -0600

    build linux libraries in steamrt-sniper for wider compatibility (due to older compilers and libraries)

--- build.sh
+++ build.sh
@@ -9,7 +9,13 @@ SCONS_CACHE=.cache/dev scons dev_build=yes "$@"
 
 # now build all the versions we're actually going to use:
 for target in debug release; do
-SCONS_CACHE=.cache/linux_$target scons target=template_$target platform=linux arch=x86_64 "$@"
+docker run --rm -ti \
+	--user "$(id -u):$(id -g)" \
+	--volume "$(pwd):$(pwd)" \
+	--workdir "$(pwd)" \
+	--env SCONS_CACHE=.cache/linux_$target \
+	registry.gitlab.steamos.cloud/steamrt/sniper/sdk \
+	scons target=template_$target platform=linux arch=x86_64 "$@"
 SCONS_CACHE=.cache/windows_$target scons target=template_$target platform=windows arch=x86_64 "$@"
 SCONS_CACHE=.cache/web_threads_$target scons target=template_$target platform=web threads=yes "$@"
 SCONS_CACHE=.cache/web_nothreads_$target scons target=template_$target platform=web threads=no "$@"
